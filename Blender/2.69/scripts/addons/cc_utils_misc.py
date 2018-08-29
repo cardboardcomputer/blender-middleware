6c3e60f88d2113e122fcb00d1eeb7d0ac96bb47e
tamask <tkemenczy@gmail.com>
2018-08-28 22:29:03 -0500

    added quicksave operator

--- Blender/2.69/scripts/addons/cc_utils_misc.py
+++ Blender/2.69/scripts/addons/cc_utils_misc.py
@@ -1,5 +1,7 @@
-import bpy
 import cc
+import os
+import bpy
+import time
 
 bl_info = {
     'name': 'Utils: Misc',
@@ -9,8 +11,39 @@ bl_info = {
     'category': 'Cardboard'
 }
 
+class Quicksave(bpy.types.Operator):
+    bl_idname = 'cc.quicksave'
+    bl_label = 'Quicksave'
+    bl_options = {'REGISTER'}
+
+    def execute(self, context):
+        dirpath = os.environ.get(
+            'BLENDER_QUICKSAVE_PATH',
+            context.user_preferences.filepaths.temporary_directory)
+        basename = bpy.path.basename(bpy.path.ensure_ext(bpy.data.filepath, ''))
+        if not basename:
+            basename = 'untitled'
+        timestamp = ('%.2f' %time.time()).replace('.', '')
+        filename = '%s.%s.blend' % (basename, timestamp)
+
+        path = os.path.join(dirpath, filename)
+
+        bpy.ops.wm.save_as_mainfile(
+            filepath=path,
+            check_existing=False,
+            compress=True,
+            copy=True,
+        )
+
+        self.report({'INFO'}, 'Quicksave: %s' % path)
+        return {'FINISHED'}
+
+__REGISTER__ = (
+    Quicksave,
+)
+
 def register():
-    pass
+    cc.utils.register(__REGISTER__)
 
 def unregister():
-    pass
+    cc.utils.unregister(__REGISTER__)
